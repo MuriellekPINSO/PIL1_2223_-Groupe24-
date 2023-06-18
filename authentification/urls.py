d386364a182e6b3bf2215f1a29aa81c0416e77fe
Murielle Kpinso <muriellekpinso@gmail.com>
2023-06-18 12:36:05 +0100

    suite de la base

--- authentification/urls.py
+++ authentification/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login', views.login, name="login"),
     path('register', views.register, name="register"),
     path('forgot-password', views.new_password, name="password.forgot"),
+    
 ]
--- home/models.py
+++ home/models.py
@@ -38,6 +38,7 @@ class Licence(models.Model):
        
 class Master(models.Model):
     nom_Master=models.CharField(25)
+    
 
     
     
